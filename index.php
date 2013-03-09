9cbc98ae4cbaed93b108f6e5d890545c693af192
Matthew Pike <matpike@gmail.com>
2013-03-09 18:06:09 +0000

    Displaying grid correctly

--- index.php
+++ index.php
@@ -75,110 +75,128 @@ input, select, textarea {
 	width: 100%;
 }
 </style>
+<script src="jquery.js" type="text/javascript"></script>
+<script src="VPT.js" type="text/javascript"></script>
+
 <script type='text/javascript'>
-var actions = new Array();
-var idx = -1;
-function start(i) {
-	if(i > actions.length) {
-		
-	}
-	var sleep = actions[i][0];
-	var func = action[i][1];
-	func();
-	if(sleep < 0) {
-		
-	} else {
-		setTimeout("start("+(i+1)+")", sleep);
-	}
-}
+    var actions = new Array();
+    var idx = -1;
+    function start(i) {
+        if (i > actions.length) {
 
-//helpers
-function next(nc) {
-	
-	if(!nc) {
-		var inputs = document.getElementsByTagName("input");
-		//console.log(inputs);
-		for(var i in inputs) {
-			//console.log(i);
-			//console.log(inputs[i]);
-			//console.log(inputs[i].style);
-			//console.log('========================================');
-			if(inputs[i].style) {
-				inputs[i].style.display = 'none';
-			}
-		}
-		document.getElementById('text').innerHTML = '';
-	}
-	
-	idx++;
-	//console.log("NEXT :: "+idx);
-	if(actions[idx][0] == "wait") {
-		EXP_wait(actions[idx][1]);
-	} else if(actions[idx][0] == "background") {
-		EXP_background(actions[idx][1]);
-	} else if(actions[idx][0] == "displayNr") {
-		EXP_displayNr(actions[idx][1], actions[idx][2], actions[idx][3]);
-	} else if(actions[idx][0] == "input") {
-		EXP_input(actions[idx][1], actions[idx][2]);
-	} else if(actions[idx][0] == "start") {
-		EXP_start();
-	} else if(actions[idx][0] == "end") {
-		EXP_end();
-	} else {
-		//console.log("NEXT :: ERROR :: "+actions[idx][0]);
-	}
-}
+        }
+        var sleep = actions[i][0];
+        var func = action[i][1];
+        func();
+        if (sleep < 0) {
 
-//actions
-function EXP_wait(time) {
-	//console.log("WAIT :: "+time);
-	setTimeout("next(false)", time*1000);
-}
+        } else {
+            setTimeout("start(" + (i + 1) + ")", sleep);
+        }
+    }
+    //helpers
+    function next(nc) {
+        if (!nc) {
+            var inputs = document.getElementsByTagName("input");
 
-function EXP_background(color) {
-	//console.log("BACKGROUND :: "+color);
-	document.getElementById('body').style.backgroundColor = color;
-	next(false);
-}
+            for (var i in inputs) {
+                //console.log(i);
+                //console.log(inputs[i]);
+                //console.log(inputs[i].style);
+                //console.log('========================================');
+                if (inputs[i].style) {
+                    inputs[i].style.display = 'none';
+                }
+            }
+            document.getElementById('text').innerHTML = '';
+        }
 
-function EXP_displayNr(name, min, max) {
-	//console.log("DISPLAY :: "+name+" - "+min+" - "+max);
-	var r = parseInt(Math.random() * (max-min));
-	r = parseInt(r) + parseInt(min);
-	document.getElementById('text').innerHTML = r;
-	//console.log("DISPLAY :: "+r);
-	var inp = "<input type='hidden' name='"+name+"_nr' value='"+r+"' />";
-	var now = document.getElementById('data').innerHTML += inp;
-	next(true);
-}
+        idx++;
+        console.log("NEXT :: " + idx);
+        if (actions[idx][0] == "wait") {
+            EXP_wait(actions[idx][1]);
+        } else if (actions[idx][0] == "background") {
+            EXP_background(actions[idx][1]);
+        } else if (actions[idx][0] == "displayNr") {
+            EXP_displayNr(actions[idx][1], actions[idx][2], actions[idx][3]);
+        } else if (actions[idx][0] == "displayGr") {
+            EXP_displayGr(actions[idx][1], actions[idx][2], actions[idx][3]);
+        } else if (actions[idx][0] == "input") {
+            EXP_input(actions[idx][1], actions[idx][2]);
+        } else if (actions[idx][0] == "start") {
+            EXP_start();
+        } else if (actions[idx][0] == "end") {
+            EXP_end();
+        } else {
+            //console.log("NEXT :: ERROR :: "+actions[idx][0]);
+        }
+    }
+    //actions
+    function EXP_wait(time) {
+        //console.log("WAIT :: "+time);
+        setTimeout("next(false)", time * 1000);
+    }
+    function EXP_background(color) {
+        //console.log("BACKGROUND :: "+color);
+        document.getElementById('body').style.backgroundColor = color;
+        next(false);
+    }
+    function EXP_displayNr(name, min, max) {
+        //console.log("DISPLAY :: "+name+" - "+min+" - "+max);
+        var r = parseInt(Math.random() * (max - min));
+        r = parseInt(r) + parseInt(min);
+        document.getElementById('text').innerHTML = r;
+        //console.log("DISPLAY :: "+r);
+        var inp = "<input type='hidden' name='" + name + "_nr' value='" + r + "' />";
+        var now = document.getElementById('data').innerHTML += inp;
+        next(true);
+    }
+    function EXP_displayGr(name, min, max) {
+        
 
-function EXP_input(name, len) {
-	//console.log("INPUT :: "+name+" - "+len);
-	var inp = "<input type='number' maxlength="+len+" size="+len+" id='elem_"+name+"' style='font-size: 80px' />";
-	var smt = "<input type='submit' value='Done' onclick='return dummySmt(\""+name+"\");' />";
-	var now = document.getElementById('data').innerHTML;
-	document.getElementById('data').innerHTML = now + inp + smt;
-}
-
-function EXP_start() {
-	//console.log("START");
-	next(false);
-}
-
-function EXP_end() {
-	//console.log("END");
-	document.getElementById('exp_form').submit();
-}
-
-function dummySmt(name) {
-	var inp = "<input type='hidden' name='"+name+"' value='"+document.getElementById('elem_'+name).value+"' />";
-	var now = document.getElementById('data').innerHTML += inp;
-	next(false);
-	return false;
-}
+        var canvas = $("#game_table tbody");
+        var Game = new VPT(canvas, 4, 4);
+        canvas.data('VPT', Game);
+        vpt = $('#game_table tbody').data('VPT');
+        Game.startGame();
+        var inp = "<input type='hidden' name='" + name + "_nr' value='" + r + "' />";
+        var now = document.getElementById('data').innerHTML += inp;
+        next(true);
+    }
+    function EXP_input(name, len) {
+        //console.log("INPUT :: "+name+" - "+len);
+        var inp = "<input type='number' maxlength=" + len + " size=" + len + " id='elem_" + name + "' style='font-size: 80px' />";
+        var smt = "<input type='submit' value='Done' onclick='return dummySmt(\"" + name + "\");' />";
+        var now = document.getElementById('data').innerHTML;
+        document.getElementById('data').innerHTML = now + inp + smt;
+    }
+    function EXP_start() {
+        //console.log("START");
+        next(false);
+    }
+    function EXP_end() {
+        //console.log("END");
+        document.getElementById('exp_form').submit();
+    }
+    function dummySmt(name) {
+        var inp = "<input type='hidden' name='" + name + "' value='" + document.getElementById('elem_' + name).value + "' />";
+        var now = document.getElementById('data').innerHTML += inp;
+        next(false);
+        return false;
+    }
 </script>
 </head>
 <body id='body' style='width: 100%' onload="next(true)">
+
+<center>
+	<table id="game_table">
+		<tbody>
+			<tr><td align="center">WAT Javascript?</td></tr>
+		</tbody>
+	</table>
+	<div id="submitButtonHere"></div>
+</center>
+
 <form id='exp_form'>
 <?php
 if(isset($_REQUEST['exp'])) {
